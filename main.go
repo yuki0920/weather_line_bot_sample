d11d69a9426406b08cd8d0bf5cb73649bf5a2e00
yuki0920 <ywsep20@gmail.com>
2021-04-09 08:42:45 +0900

    Add initial message

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	// LINE Botクライアント生成する
+	// BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// テキストメッセージを生成する
+	message := linebot.NewTextMessage("hello, world")
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+}
