f891af78fc190615cc512c7beadf8fb3af0f34b9
yuki0920 <ywsep20@gmail.com>
2021-04-10 08:31:04 +0900

    Use weather package

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/line/line-bot-sdk-go/linebot"
+	"github.com/yuki0920/weather_line_bot_sample/weather"
 )
 
 func main() {
@@ -18,8 +19,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// weatherパッケージパッケージから天気情報の文字列をを取得する
+	result := weather.GetWeather()
 	// テキストメッセージを生成する
-	message := linebot.NewTextMessage("hello, world")
+	message := linebot.NewTextMessage(result)
 	// テキストメッセージを友達登録しているユーザー全員に配信する
 	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
 		log.Fatal(err)
